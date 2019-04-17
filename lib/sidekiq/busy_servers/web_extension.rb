fa0a99fdef31adb8adeabb301efa2058fd1abdfc
Viktor Nazarenko <victorn.ua@gmail.com>
2019-04-17 21:07:13 +0300

    show versioons as separate table

--- lib/sidekiq/busy_servers/web_extension.rb
+++ lib/sidekiq/busy_servers/web_extension.rb
@@ -9,17 +9,24 @@ module Sidekiq
           @processes = processes
           @processes.each do |p|
             p['queues'].each do |p_name|
-              process_queues[p_name] ||= {concurrency: 0, busy: 0}
+              process_queues[p_name] ||= {concurrency: 0, busy: 0, processes: 0}
               process_queues[p_name][:concurrency] += p['concurrency']
               process_queues[p_name][:busy] += p['busy']
+              process_queues[p_name][:processes] += 1
             end
           end
           @queue_data = []
+          @queue_names = []
           Sidekiq::Queue.all.each do |queue|
-            #next unless process_queues[queue.name]
+            next unless process_queues[queue.name]
+            @queue_names << queue.name
             @queue_data << {name: queue.name, size: queue.size, concurrency: process_queues[queue.name][:concurrency], busy: process_queues[queue.name][:busy], paused: queue.paused?}
           end
-          puts @queue_data.inspect
+          @versions = []
+          process_queues.each do |p_name, p_data|
+            next if p_name.to_s.match?("bot") || @queue_names.include?(p_name)
+            @versions << {name: p_name, processes: p_data[:processes], concurrency: p_data[:concurrency], busy: p_data[:busy]}
+          end
 
           render(:erb, File.read("#{ROOT}/views/busy_servers.erb"))
         end
